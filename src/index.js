d349b07c59d886ec086f2c26ef580b9a32a8c53a
Xiaotong <u7709227@anu.edu.au>
2025-01-08 16:32:28 +0800

    use redux to protect token data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.scss';
-import { RouterProvider } from 'react-router-dom';
-import router from './router';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.scss'
+import { RouterProvider } from 'react-router-dom'
+import router from './router'
+import { Provider } from 'react-redux'
+import store from './store'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <React.StrictMode>
+
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </React.StrictMode>
-);
+  </Provider>
+
+)
+
 
