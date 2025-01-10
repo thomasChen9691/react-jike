bcafd1d9768c50ac74db10568a4d7fe1aa9b5194
Xiaotong <u7709227@anu.edu.au>
2025-01-10 23:29:06 +0800

    test

--- src/utils/request.js
+++ src/utils/request.js
@@ -7,7 +7,7 @@ import router from "@/router"
 // 3. 请求拦截器 / 响应拦截器
 
 const request = axios.create({
-  baseURL: 'http://geek.itheima.net/v1_0',
+  baseURL: 'https://geek.itheima.net/v1_0',
   timeout: 5000
 })
 
